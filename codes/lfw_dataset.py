266aa88240bb9aaad1b39e39bbdfdbe455bb3673
DinhNguyen <nguyendinh1987@gmail.com>
2020-09-23 18:50:13 +0800

    update lfw_dataset

--- codes/lfw_dataset.py
+++ codes/lfw_dataset.py
@@ -4,11 +4,12 @@ import cv2
 import glob
 import sys, os
 from sklearn.utils import shuffle
+import copy
+import face_recognition # install by "pip install face-recognition"
 
 class lfw_face(object):
-    def __init__(self,rootF,**kargs):
-        assert rootF is not None, "No path to dataset"
-        self.rootFo = rootF
+    def __init__(self,**kargs):
+        self.rootFo = kargs["fullfaceroot"]
         self.verbose = True if "verbose" not in kargs.keys() else kargs["verbose"]
         self.outFo = "/opt" if "prog_output" not in kargs.keys() else kargs["prog_output"]
         self.useAllClasses = False if "use_all" not in kargs.keys() else kargs["use_all"]
@@ -30,6 +31,8 @@ class lfw_face(object):
             self.nFocusClasses = self.nAllClasses
             if self.verbose:
                 print("Focus classes has {} identified people.".format(self.nFocusClasses))
+                print("###################################################################################")
+                print(" ")
 
         else:
             self._getFocusLabels()
@@ -39,8 +42,10 @@ class lfw_face(object):
             nc = [len(l) for l in self.faceList]
             n = np.sum(np.array(nc))
             print("There are {} full face images for {} identified faces".format(n,self.nFocusClasses))
+            print("###################################################################################")
+            print(" ")
         
-        self.maskedFaceRootFo = None if "maskedface" not in kargs.keys() else kargs["maskedface"]
+        self.maskedFaceRootFo = None if "maskedfaceroot" not in kargs.keys() else kargs["maskedfaceroot"]
         if self.maskedFaceRootFo is not None:
             self.maskfaceList = self._getFaceList(group="maskface")
             for index in range(len(self.maskfaceList)):
@@ -49,58 +54,10 @@ class lfw_face(object):
                 nc = [len(l) for l in self.maskfaceList]
                 n = np.sum(np.array(nc))
                 print("There are {} masked face images for {} identified faces".format(n,self.nFocusClasses))
+                print("###################################################################################")
+                print(" ")
         else:
             self.maskfaceList = None
-    #####
-    def getFaceList(self,group="face",fullpath=False,withlabelId=False):
-        if fullpath:
-            tmp = self.faceList if group=="face" else self.maskfaceList
-            rootFo = self.rootFo if group=="face" else self.maskedFaceRootFo
-            Flist = []
-            for l in tmp:
-                for lf in l:
-                    Flist.append(os.path.join(rootFo,lf))
-        else: 
-            Flist = self.faceList if group=="face" else self.maskfaceList
-        if withlabelId:
-            # labelIds = []
-            # for l in Flist:
-            #     print(l)
-            #     labelIds.append(self._getId(l.split("/")[-2]))
-            labelIds = [self._getId(l.split("/")[-2]) for l in Flist]
-            return [Flist,labelIds]
-        else:
-            return Flist
-    def getnClasses(self):
-        return self.nAllClasses
-    def getAllIds(self):
-        idList = np.zeros((1,len(self.focusLabels)))
-        idx = -1
-        for k,v in self.focusLabels.items():
-            idx += 1
-            idList[0,idx] = int(v)
-        return idList
-    def getId(self,labels):
-        return [self._getId(l) for l in labels]
-        
-    def getRefList(self,nr=1,group="face",fullpath=False):
-        fList = self.faceList if group=="face" else self.maskfaceList
-        rootFo= self.rootFo if group=="face" else self.maskedFaceRootFo
-        ns = [len(l) for l in fList]
-        min_ns = min(nr,min(ns))
-        refIList = []
-        refLList = []
-        for l in fList:
-            samples = rng.choice(len(l),min_ns,replace=False)
-            for s in samples:
-                if fullpath:
-                    refIList.append(os.path.join(rootFo,l[s]))
-                else:
-                    refIList.append(l[s])
-                refLList.append(self._getId(l[s].split("/")[0]))
-
-        return [refIList,refLList],min_ns
-    
     #####
     def _getId(self,label):
         return self.focusLabels[label]
@@ -134,6 +91,8 @@ class lfw_face(object):
         #     print("{}: {}".format(k,v))
         if self.verbose:
             print("LFW has {} identified people.".format(self.nAllClasses))
+            print("###################################################################################")
+            print(" ")
                 
         return True
 
@@ -180,7 +139,8 @@ class lfw_face(object):
         
         if self.verbose:
             print("Focus classes has {} identified people.".format(self.nFocusClasses))
-
+            print("###################################################################################")
+            print(" ")
         return True
     
     def _getFaceList(self,group="face"):
@@ -203,7 +163,53 @@ class lfw_face(object):
         faceList = [tmp[i] for i in range(self.nAllClasses) if len(tmp[i])>0]
         
         return faceList
+    #####
+    def getFaceList(self,group="face",fullpath=False,withlabelId=False):
+        if fullpath:
+            tmp = self.faceList if group=="face" else self.maskfaceList
+            rootFo = self.rootFo if group=="face" else self.maskedFaceRootFo
+            Flist = []
+            for faceid in range(len(tmp)):
+                Flist.append([os.path.join(rootFo,lf) for lf in tmp[faceid]])
+        else: 
+            Flist = self.faceList if group=="face" else self.maskfaceList
+        if withlabelId:
+            # labelIds = []
+            # for l in Flist:
+            #     print(l)
+            #     labelIds.append(self._getId(l.split("/")[-2]))
+            labelIds = [self._getId(l[0].split("/")[-2]) for l in Flist]
+            return [Flist,labelIds]
+        else:
+            return [Flist]
+    def getnClasses(self):
+        return self.nAllClasses
+    def getAllIds(self):
+        idList = np.zeros((1,len(self.focusLabels)))
+        idx = -1
+        for k,v in self.focusLabels.items():
+            idx += 1
+            idList[0,idx] = int(v)
+        return idList
+    def getId(self,labels):
+        return [self._getId(l) for l in labels]   
+    def getRefList(self,nr=1,group="face",fullpath=False):
+        fList = self.faceList if group=="face" else self.maskfaceList
+        rootFo= self.rootFo if group=="face" else self.maskedFaceRootFo
+        ns = [len(l) for l in fList]
+        min_ns = min(nr,min(ns))
+        refIList = []
+        refLList = []
+        for l in fList:
+            samples = rng.choice(len(l),min_ns,replace=False)
+            for s in samples:
+                if fullpath:
+                    refIList.append(os.path.join(rootFo,l[s]))
+                else:
+                    refIList.append(l[s])
+                refLList.append(self._getId(l[s].split("/")[0]))
 
+        return [refIList,refLList],min_ns 
     def load_samples(self,samples=None,**kargs):
         if samples is None:
             print("No samples to load")
@@ -213,8 +219,7 @@ class lfw_face(object):
         for sid,s in enumerate(samples):
             imgs[sid],labels[sid] = self.load_sample(sample=s,**kargs)
         return imgs,labels
-
-    def load_sample(self,sample=None,face_type="face",wh=None,isshow=False):
+    def load_sample(self,sample=None,subpath=None,face_type="face",wh=None,isshow=False):
         '''
         sample: is in format "label/img.jpg"
         '''
@@ -225,11 +230,55 @@ class lfw_face(object):
         if wh is not None:
             I = cv2.resize(I,(wh[1],wh[0]))
         label = sample.split("/")[0]
+        if subpath == "up-half":
+            h = I.shape[0]
+            I = I[:int(h/2),:,:]
+        elif subpath == "down-half":
+            h = I.shape[0]
+            I = I[:int(h/2),:,:]                        
+        else:
+            pass
         if isshow:
             cv2.imshow(label,I)
             cv2.waitKey()
             cv2.destroyWindow(label)
         return I, label
+    def facelocalization(self,sample,model="hog",wh=None,isshow=False):
+        '''
+        Should not use this function with cropped face
+        '''
+        I = face_recognition.load_image_file(sample)
+        face_locations = face_recognition.face_locations(I, model=model)
+        if isshow:
+            I2s = copy.copy(I)
+            I2s = cv2.cvtColor(I2s, cv2.COLOR_RGB2BGR)
+            color = (255, 0, 0)
+            thickness = 2
+            for lo in face_locations:
+                start_point = (lo[0],lo[1])
+                end_point = (lo[2],lo[3])
+                I2s = cv2.rectangle(I2s, start_point, end_point, color, thickness) 
+            label = sample.split("/")[0]
+            cv2.imshow(label,I2s)
+            cv2.waitKey()
+            cv2.destroyWindow(label)
+        return I,face_locations
+    
+    def facelandmark(self,sample,model="hog",wh=None,isshow=False):
+        I = face_recognition.load_image_file(sample)
+        face_landmarks_list = face_recognition.face_landmarks(I)
+        for item in face_landmarks_list:
+            for k,v in item.items():
+                print(k)
+                for p in v:
+                    centerOfCircle = p
+                    radius = 10
+                    color = [255,255,0]
+                    thickness = 5
+                    I = cv2.circle(I, centerOfCircle, radius, color, thickness)
+                cv2.imshow("test",I)
+                cv2.waitKey()
+                cv2.destroyWindow("test")
     
     ######################################################
     def full_mask_face_pair_gen(self,**kargs):
